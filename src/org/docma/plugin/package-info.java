6e39935f08cd9331ea34ae7e34bac72eaef408b6
MP <>
2017-05-15 21:50:20 +0200

    Finished version 1.9.

--- src/org/docma/plugin/package-info.java
+++ src/org/docma/plugin/package-info.java
@@ -1,12 +1,10 @@
 
 /**
- * Provides the classes required to create Auto-Format plug-ins for the
- * Docmenta application.
- * To create an Auto-Format Plug-in, create a Java class that implements the
- * {@link org.docma.plugin.AutoFormat} interface.
- * For an introduction of the Auto-Format concept see the
- * <a href="doc-files/autoformat_overview.html">Auto-Format - Overview and Example</a>
- * page.
+ * Provides the classes and interfaces to create a Docmenta plug-in.
+ * 
+ * <p>
+ * For an introduction of the Plug-in API, see the "Plug-in Developer Guide".
+ * </p>
  */
 
 package org.docma.plugin;
